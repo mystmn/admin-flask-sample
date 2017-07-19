e0534250c06289ed5583950a1f7a6eca8ff9a616
pcameron <pcameron5@gmail.com>
2017-07-19 19:50:21 -0400

    saving results

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, render_template, request
 from flask_admin import BaseView, Admin, expose
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin.contrib import sqla
-import random
 
 # Create flask app
 app = Flask(__name__, template_folder='templates')
@@ -17,12 +16,36 @@ db = SQLAlchemy(app)
 app.debug = True
 
 
-class NetworkDevices(db.Model):
+#  Saving the Segments for easy access
+class SegmentSQL(db.Model):
+    __tablename__ = 'segments'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ip_address = db.Column(db.String(11))
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __str__(self):
+        return self.desc
+
+
+class SegmentModel(sqla.ModelView):
+    column_display_pk = False  # Controls if the primary key should be displayed in the list view.
+    column_searchable_list = ['id', 'ip_address']
+    form_columns = ['id', 'ip_address', 'created_on']
+
+    form_widget_args = {
+        'created_on': {
+            'disabled': True
+        }
+    }
+
+
+# Network section ##
+class NetworkSQL(db.Model):
     __tablename__ = 'deviceNames'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     device_name = db.Column(db.String(25))  # unique=False
     location = db.Column(db.String(50))
-    desc = db.Column(db.String(50))
+    ip_address = db.Column(db.String(50))
     created_on = db.Column(db.DateTime, server_default=db.func.now())
     updated_on = db.Column(db.DateTime(), server_default=db.func.now(), onupdate=db.func.now())
 
@@ -30,30 +53,22 @@ class NetworkDevices(db.Model):
         return self.desc
 
 
-class NetworkAdmin(sqla.ModelView):
+class NetworkModel(sqla.ModelView):
     column_display_pk = False  # Controls if the primary key should be displayed in the list view.
-    column_searchable_list = ['device_name', 'desc']
-    form_columns = ['device_name', 'location', 'desc', 'created_on', 'updated_on']
+    column_searchable_list = ['device_name', 'ip_address']
+    form_columns = ['device_name', 'location', 'ip_address', 'created_on', 'updated_on']
 
     form_widget_args = {
         'desc': {
             'rows': 10,
             'style': 'color: red'
-        }
-    }
-
-    form_widget_args = {
+        },
         'created_on': {
             'disabled': True
         }, 'updated_on': {
             'disabled': True
         }
     }
-
-    def create_form(self):
-        form = NetworkDevices()
-        form.vendor.query = device_name.query.all()
-        return form
 
 
 def build_live_list():
@@ -68,16 +83,31 @@ def build_live_list():
     #  print(s.read())
     each_line_scanned = nmap3.filter_names(s)
 
-    for more in each_line_scanned[45:60]:
-        each = NetworkDevices()
-        each.device_name = more['Device']
+    for more in each_line_scanned[:5]:
+        device_each = NetworkSQL()
 
-        #  Join list[xxx, xxx, xxx, xxx]
+        if more['Device'] is str("DHCP"):
+            device_each.device_name = " "
+        else:
+            device_each.device_name = more['Device']
+
+        # Join list[xxx, xxx, xxx, xxx]
         ip = ".".join(more['IP'])
-        each.desc = ip
 
-        each.location = "N/A"
-        db.session.add(each)
+        device_each.ip_address = ip
+        device_each.location = "N/A"
+        db.session.add(device_each)
+
+        #  Scan for liveNetwork segment...must not have duplicates
+        seg = SegmentSQL()
+        unique_segment = ".".join(more['IP'][:3])  # Only the first 3 octets
+        seg.ip_address = unique_segment
+
+        testing_setting = db.session.query(SegmentSQL).filter_by(ip_address=unique_segment).first()
+
+        if not testing_setting:
+            db.session.add(seg)
+
         db.session.commit()
 
 
@@ -90,20 +120,35 @@ def index():
 @app.route('/build')
 def building():
     build_live_list()
-    return redirect('admin/networkdevices')
+    return redirect('admin/networksql')
+
+
+@app.route('/n/segment')
+def tesingpath():
+    names = request.args.get('name')
+    return render_template('octets.html', names=names)
+
+
+@app.route('/segment')
+def segmentSQL():
+    names = NetworkSQL.query.all()
+    return render_template('octets.html', names=names)
 
 
 # Create custom admin view
-class MyAdminView(BaseView):
+class MyView(BaseView):
     @expose('/')
     def index(self):
-        return self.render('myadmin.html')
+        name = "bob"
+        return self.render('myadmin.html', name=name)
 
 
 # Create admin interface
 admin = Admin(name="Pacman", template_mode='bootstrap3')
-admin.add_view(NetworkAdmin(NetworkDevices, db.session))
-admin.add_view(MyAdminView(name="Accounts", category='Custom'))
+admin.add_view(MyView(name='My View', menu_icon_type='glyph', menu_icon_value='glyphicon-home'))
+admin.add_view(NetworkModel(NetworkSQL, db.session))
+admin.add_view(SegmentModel(SegmentSQL, db.session))
+
 admin.init_app(app)
 
 if __name__ == '__main__':
@@ -115,4 +160,4 @@ if __name__ == '__main__':
     db.create_all()
 
     # Start app
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=5001)
